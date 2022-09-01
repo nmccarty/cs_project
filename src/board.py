1ab1d1485f10a8ad0e378df91144bfcf16db460f
Shaurya Shubham <me@shauryashubham.com>
2022-09-01 20:48:40 +0530

    add grid class

--- src/board.py
+++ src/board.py
@@ -12,30 +12,62 @@ class Piece:
     color: Color
 
 
-def create_empty_board_grid():
-    grid = [None] * 64
-    grid[0] = Piece(PieceType.Rook, Color.White)
-    grid[7] = Piece(PieceType.Rook, Color.White)
+@dataclass
+class Grid:
+    inner: list = field(init=false, default=[None] * 64)
 
-    grid[1] = Piece(PieceType.Knight, Color.White)
-    grid[6] = Piece(PieceType.Knight, Color.White)
+    def __post_init__(self):
+        self[0, 0] = Piece(PieceType.Rook, Color.White)
+        self[0, 7] = Piece(PieceType.Rook, Color.White)
 
-    grid[2] = Piece(PieceType.Bishop, Color.White)
-    grid[5] = Piece(PieceType.Bishop, Color.White)
+        self[0, 1] = Piece(PieceType.Knight, Color.White)
+        self[0, 6] = Piece(PieceType.Knight, Color.White)
 
-    grid[3] = Piece(PieceType.Queen, Color.White)
+        self[0, 2] = Piece(PieceType.Bishop, Color.White)
+        self[0, 5] = Piece(PieceType.Bishop, Color.White)
 
-    grid[4] = Piece(PieceType.King, Color.White)
+        self[0, 3] = Piece(PieceType.Queen, Color.White)
 
-    grid[8 : 8 + 8] = [Piece(PieceType.Pawn, Color.White)] * 8
-    return grid
+        self[0, 4] = Piece(PieceType.King, Color.White)
+
+        self[7, 0] = Piece(PieceType.Rook, Color.Black)
+        self[7, 7] = Piece(PieceType.Rook, Color.Black)
+
+        self[7, 1] = Piece(PieceType.Knight, Color.Black)
+        self[7, 6] = Piece(PieceType.Knight, Color.Black)
+
+        self[7, 2] = Piece(PieceType.Bishop, Color.Black)
+        self[7, 5] = Piece(PieceType.Bishop, Color.Black)
+
+        self[7, 3] = Piece(PieceType.Queen, Color.Black)
+
+        self[7, 4] = Piece(PieceType.King, Color.Black)
+
+        for i in range(8):
+            self[1, i] = Piece(PieceType.Pawn, Color.White)
+            self[6, i] = Piece(PieceType.Pawn, Color.Black)
+
+    def __getitem__(self, i):
+        return self.inner[self.__index_to_num(i)]
+
+    def __setitem__(self, i, data):
+        self.inner[self.__index_to_num(i)] = data
+
+    def __index_to_num(i):
+        if type(i) is tuple:
+            r, c = i
+            return r * 8 + c
+        elif type(i) is str:
+            return (int(i[1]) - 1) * 8 + ord(i[0]) - ord("a")
+        else:
+            return None
 
 
 @dataclass
 class Board:
-    grid: field(default_factory=create_empty_board_grid)
+    grid: Grid
     black_victims: list
     white_victims: list
 
 
-print(create_empty_board_grid())
+print(Board())
