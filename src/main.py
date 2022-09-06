369e21984940d28e89a96b5ea7848a13fd9e6d92
Shaurya Shubham <me@shauryashubham.com>
2022-09-06 22:38:25 +0530

    'update'

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
     QVBoxLayout,
+    QHBoxLayout,
     QPushButton,
     QLabel,
     QWidget,
@@ -24,7 +25,7 @@ def main():
     sys.exit(app.exec())
 
 
-PlayerType = enum("PlayerType", "Human Computer")
+PlayerType = Enum("PlayerType", "Human Computer")
 
 
 @dataclass
@@ -38,31 +39,37 @@ class GameState:
 @dataclass
 class StartState:
     def construct_widget(self, parent):
-        widget = QWidget()
-        container = QWidget()
+        container_a = QWidget()
+        container_b = QWidget()
+        container_a_layout = QHBoxLayout()
+        container_a_layout.addWidget(container_b)
+        container_a.setLayout(container_a_layout)
+
+        container_b_layout = QVBoxLayout()
+        container_b.setLayout(container_b_layout)
+        return container_a
 
 
-
-StageTag = enum("StageTag", "Start Game")
+StageTag = Enum("StageTag", "Start Game")
 
 
 @dataclass
 class State:
     tag: StageTag = field(default=StageTag.Start)
-    inner: Any = field(default_factory=StartState)
+    inner: any = field(default_factory=StartState)
 
     def construct_widget(self, parent):
-        return inner.construct_widget(parent)
+        return self.inner.construct_widget(parent)
 
 
 @dataclass
 class MainWindow:
     qwindow: QMainWindow = field(default_factory=QMainWindow)
-    state: State = field(default=State.StartState())
+    state: State = field(default_factory=State)
 
     def start(self):
-        self.qwindow = self.state.construct_widget()
-        qwindow.show()
+        self.qwindow = self.state.construct_widget(None)
+        self.qwindow.show()
 
 
 main()
